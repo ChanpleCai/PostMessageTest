4b324939d0f827c5071286979b4142809fa1ea12
Chanple <chanple@outlook.com>
2019-02-25 22:30:27 +0800

    ~

---
+++ PostMessageTest/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PostMessageTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Program.FindWindow("Shell_TrayWnd", null).ToString("x8");
+            textBox2.Text = 0x05CB.ToString("x8");
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.PostMessageW((IntPtr) Convert.ToInt64(textBox1.Text, 16), Convert.ToUInt32(textBox2.Text, 16),
+                (IntPtr) Convert.ToInt64(textBox3.Text, 16), (IntPtr) Convert.ToInt64(textBox4.Text, 16));
+        }
+    }
+}
---
+++ PostMessageTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace PostMessageTest
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        /// Return Type: BOOL->int
+        /// hWnd: HWND->HWND__*
+        /// Msg: UINT->unsigned int
+        /// wParam: WPARAM->UINT_PTR->unsigned int
+        /// lParam: LPARAM->LONG_PTR->int
+        [DllImport("user32.dll", EntryPoint = "PostMessageW")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool PostMessageW(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        /// Return Type: HWND->HWND__*
+        ///lpClassName: LPCWSTR->WCHAR*
+        ///lpWindowName: LPCWSTR->WCHAR*
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+    }
+}
