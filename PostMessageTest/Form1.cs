28ade9ae5af5c858fbe6dad3b82a5b0ef88bd9da
Chanple Cai <chanple@outlook.com>
2019-09-24 11:36:22 +0800

    1903 Taskbar Message

--- PostMessageTest/Form1.cs
+++ PostMessageTest/Form1.cs
@@ -9,15 +9,15 @@ namespace PostMessageTest
         {
             InitializeComponent();
             textBox1.Text = Program.FindWindow("Shell_TrayWnd", null).ToString("x8");
-            textBox2.Text = 0x05CB.ToString("x8");
+            textBox2.Text = 0x05D1.ToString("x8");
             textBox3.Text = "0";
             textBox4.Text = "0";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Program.PostMessageW((IntPtr) Convert.ToInt64(textBox1.Text, 16), Convert.ToUInt32(textBox2.Text, 16),
-                (IntPtr) Convert.ToInt64(textBox3.Text, 16), (IntPtr) Convert.ToInt64(textBox4.Text, 16));
+            Program.PostMessageW((IntPtr)Convert.ToInt64(textBox1.Text, 16), Convert.ToUInt32(textBox2.Text, 16),
+                (IntPtr)Convert.ToInt64(textBox3.Text, 16), (IntPtr)Convert.ToInt64(textBox4.Text, 16));
         }
     }
 }
